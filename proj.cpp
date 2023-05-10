c40bae4da7b370979e5083ffcb7330d358348bc2
Simon Henry <simonrapaka@gmail.com>
2023-05-10 23:41:11 +0530

    Added Game over loop and pause screen help text

--- proj.cpp
+++ proj.cpp
@@ -7,10 +7,10 @@ using namespace std;
 int fail=0;
 
 //For Windowed Operation
-int SW = 640, SH = 640, w = 4,m = 0;
+// int SW = 640, SH = 640, w = 4,m = 0;
 
 //For FullScreen Operation
-// int SW = 1280,SH = 720,w = 1,m = 1;
+int SW = 1280,SH = 720,w = 1,m = 1;
 
 class Snake
 {
@@ -21,7 +21,7 @@ public:
 	Snake();
 	void update(int d);
 	void render(SDL_Renderer *r);
-	void render(SDL_Renderer *r, TTF_Font *font, SDL_Color *col, SDL_Surface *surf, SDL_Texture *text, SDL_Rect *score, bool *p,char *s);
+	void render(SDL_Renderer *r, TTF_Font *font, SDL_Color *col, SDL_Surface *surf, SDL_Texture *text, SDL_Rect *score, bool *p,const char *s);
 	void renderf(SDL_Renderer *r);
 	int score();
 };
@@ -32,7 +32,7 @@ Snake::Snake()
 		s[i] = {0,0,0,0};
 
 	size = 16; //sets the size of each cell
-	l=3; //sets the length
+	l=15; //sets the length
 
 	int x = ((rand() % SW)>>4)<<4; //generates a random location for the food
 	int y = ((rand() % SH)>>4)<<4;
@@ -149,7 +149,7 @@ void Snake::render(SDL_Renderer *r)
 	SDL_RenderFillRect(r,&s[0]); //render head of snake
 }
 
-void Snake::render(SDL_Renderer *r, TTF_Font *font, SDL_Color *col, SDL_Surface *surf, SDL_Texture *text, SDL_Rect *score, bool *p,char *s)
+void Snake::render(SDL_Renderer *r, TTF_Font *font, SDL_Color *col, SDL_Surface *surf, SDL_Texture *text, SDL_Rect *score, bool *p,const char *s)
 {
 	surf = TTF_RenderText_Solid(font,s,*col);
 	text = SDL_CreateTextureFromSurface(r,surf);	
@@ -181,14 +181,7 @@ int main(int argc, char *argv[])
 		else cout << "Mouse Capture OK\n";
 	}
 
-	int run = 1,fps = 60,dir = 1,del=0; // initialize with direction up and 0ms delay
-
-	bool p = false; //pause flag set to false
-
-	Snake s1; //Create Snake object
-	SDL_Event e; //Create an event to check inputs
-
-	int scw = 150, sch = 30; //Score rectangle widht and height
+	int scw = 190, sch = 30; //Score rectangle widht and height
 	int pw = 300, ph = 80; //Pause text rectangle width and height
 	int spw = 190, sph = 30;
 	SDL_Rect r = {0,32,SW,SH-32}; //Screen size rectangle for dimming
@@ -198,10 +191,13 @@ int main(int argc, char *argv[])
 	SDL_Rect speed = {SW-spw-10,4,spw,sph};
 
 	TTF_Font *font =  TTF_OpenFont("Minecrafter.Reg.ttf",14); //Load Font
-	if(font==NULL) {cout << "Font not found\n"; run = 0;} //Check if font is loaded or not
+	if(font==NULL) {cout << "Font not found\n"; return 0;} //Check if font is loaded or not
 
 	TTF_Font *pfont = TTF_OpenFont("Minecrafter.Reg.ttf",24);
-	if(pfont==NULL) {cout << "Font not found\n"; run =0;}
+	if(pfont==NULL) {cout << "Font not found\n"; return 0;}
+
+	TTF_Font *hfont = TTF_OpenFont("PixeloidSans-mLxMm.ttf",14); //Pause screen font
+	if(hfont==NULL) {cout << "Font not found\n"; return 0;}
 
 	SDL_Color scol = {80,80,80}, pcol = {200,200,200}; //Font color
 	SDL_Surface *surf; //Surface for the font to load to
@@ -211,110 +207,168 @@ int main(int argc, char *argv[])
 
 	char pz[] = "PAUSED";
 
-	while(run)
+	SDL_Rect ht[8]; //Locations for the pause screen text
+
+	ht[0] = {SW/2-130,pause.y + 100,260,30};
+	ht[1] = {SW/2-50,ht[0].y + 80,100,30};
+	ht[2] = {SW/2-35,ht[1].y + 30,70,30};
+	ht[3] = {SW/2-40,ht[2].y + 30,80,30};
+	ht[4] = {SW/2-45,ht[3].y + 30,90,30};
+	ht[5] = {SW/2-40,ht[4].y + 30,80,30};
+	ht[6] = {SW/2-100,ht[5].y + 70,200,30};
+	ht[7] = {SW/2-150,ht[6].y + 40,300,30};
+
+	string help[8];
+
+	help[0] = "Press Esc to resume";
+	help[1] = "Controls";
+	help[2] = "W: Up";
+	help[3] = "A: Right";
+	help[4] = "S: Down";
+	help[5] = "D: Left";
+	help[6] = "Press X to Exit";
+	help[7] = "Press Enter to Continue";
+
+	string go[3];
+
+	go[0] = "Game"; go[1] = "Over";
+
+	SDL_Rect go2[3];
+
+	go2[0] = {SW/2-140,pause.y + 100,280,80};
+	go2[1] = {SW/2-120,go2[0].y + 100,240,80};
+	go2[2] = {SW/2-110,go2[1].y + 100,220,30};
+
+	SDL_Color gocol = {200,200,200};
+
+	int runmain=1;
+
+	while(runmain)
 	{
-		int start = SDL_GetTicks();
-		while(SDL_PollEvent(&e) != 0)
-		{
-			if(e.type == SDL_QUIT) //if the close button or Alt+F4 is pressed
-				run = 0;
+		int run = 1,fps = 60,dir = 1,del=0; // initialize with direction up and 0ms delay
+		int cont = 0;//continue flag
+		bool p = false; //pause flag set to false
 
-			if(e.type == SDL_KEYDOWN) //if a key is pressed
+		Snake s1; //Create Snake object
+		SDL_Event e; //Create an event to check inputs
+
+
+		while(run)
+		{
+			int start = SDL_GetTicks();
+			while(SDL_PollEvent(&e) != 0)
 			{
-				if(e.key.keysym.scancode == SDL_SCANCODE_W)
-					{if(dir!=3 && !p) dir = 1; break;}
-				
-				if(e.key.keysym.scancode == SDL_SCANCODE_D)
-					{if(dir!=4 && !p) dir = 2; break;}
-				
-				if(e.key.keysym.scancode == SDL_SCANCODE_S)
-					{if(dir!=1 && !p) dir = 3; break;}
-				
-				if(e.key.keysym.scancode == SDL_SCANCODE_A)	
-					{if(dir!=2 && !p) dir = 4; break;}
+				if(e.type == SDL_QUIT) //if the close button or Alt+F4 is pressed
+					run = 0;
 
-				if(e.key.keysym.scancode == SDL_SCANCODE_ESCAPE) //if escape is pressed while running, set pause flag to true, if pressed while paused, set to false
-					p = !p;
+				if(e.type == SDL_KEYDOWN) //if a key is pressed
+				{
+					if(e.key.keysym.scancode == SDL_SCANCODE_W)
+						{if(dir!=3 && !p) dir = 1; break;}
+					
+					if(e.key.keysym.scancode == SDL_SCANCODE_D)
+						{if(dir!=4 && !p) dir = 2; break;}
+					
+					if(e.key.keysym.scancode == SDL_SCANCODE_S)
+						{if(dir!=1 && !p) dir = 3; break;}
+					
+					if(e.key.keysym.scancode == SDL_SCANCODE_A)	
+						{if(dir!=2 && !p) dir = 4; break;}
 
-				if(e.key.keysym.scancode == SDL_SCANCODE_X) //if x is pressed while paused set run flag to zero exiting the game
-					if(p) run = 0;
+					if(e.key.keysym.scancode == SDL_SCANCODE_ESCAPE) //if escape is pressed while running, set pause flag to true, if pressed while paused, set to false
+						p = !p;
+
+					if(e.key.keysym.scancode == SDL_SCANCODE_X) //if x is pressed while paused set run flag to zero exiting the game
+						if(p || fail) run = 0;
+
+					if(e.key.keysym.scancode == SDL_SCANCODE_RETURN)
+						if(fail) {fail=0;cont=1;}
+				}
+				
+				if(e.type == SDL_MOUSEWHEEL)
+				{
+					if(e.wheel.y < 0) //if scroll wheel is scrolled down increase the delay after rendering each frame slowin down the game
+						if(del+2<=200)
+							del+=2;
+					if(e.wheel.y > 0) //if scroll wheel is scrolled up decrease the delay, speeding up the game
+						if(del-2 >= 0)
+							del-=2;
+				}
 			}
-			
-			if(e.type == SDL_MOUSEWHEEL)
+			if(!run) break;
+			if(cont) {run=0;continue;}
+			SDL_SetRenderDrawColor(ren,0x98,0x80,0x4F,255); //Render Background
+			SDL_RenderClear(ren);
+
+			SDL_SetRenderDrawColor(ren,0x1D,0xC9,0x5C,255); //Set color to a light green
+
+			if(!fail) s1.render(ren); //Render snake, snake head and food
+
+			SDL_SetRenderDrawColor(ren,0,180,150,255);
+			SDL_RenderFillRect(ren,&bar);
+
+			char sc[15];
+
+			sprintf(sc,"Score  %d", s1.score());
+
+			s1.render(ren,font,&scol,surf,text,&score,&p,sc); // Render Score Text
+
+			char sp[20];
+
+			sprintf(sp,"Speed  %d",((del-200)/2)*(-1));
+
+			s1.render(ren,font,&scol,surf,text,&speed,&p,sp);
+
+			if(p) //if paused dim the screen
 			{
-				if(e.wheel.y < 0) //if scroll wheel is scrolled down increase the delay after rendering each frame slowin down the game
-					if(del+2<=200)
-						del+=2;
-				if(e.wheel.y > 0) //if scroll wheel is scrolled up decrease the delay, speeding up the game
-					if(del-2 >= 0)
-						del-=2;
+				SDL_SetRenderDrawBlendMode(ren,SDL_BLENDMODE_BLEND);
+				SDL_SetRenderDrawColor(ren,0,0,0,100);
+				SDL_RenderFillRect(ren,&r);
+
+				s1.render(ren,pfont,&pcol,surf,text,&pause,&p,pz);
+				for(int i=0;i<7;i++)
+					s1.render(ren,hfont,&pcol,surf,text,&ht[i],&p,help[i].c_str());
 			}
+
+			if(fail)
+			{
+				//Render Snake in red
+				s1.renderf(ren);
+
+				//Render a red overlay on the screen
+				SDL_SetRenderDrawBlendMode(ren,SDL_BLENDMODE_BLEND); 
+				SDL_SetRenderDrawColor(ren,120,0,0,100);
+				SDL_RenderFillRect(ren,&r);
+
+				for(int i=0;i<2;i++)
+					s1.render(ren,pfont,&gocol,surf,text,&go2[i],&p,go[i].c_str());
+
+				char gosc[20];
+
+				sprintf(gosc,"Final Score: %d", s1.score());
+
+				s1.render(ren,hfont,&gocol,surf,text,&go2[2],&p,gosc);
+
+				for(int i=6;i<8;i++)
+					s1.render(ren,hfont,&gocol,surf,text,&ht[i],&p,help[i].c_str());
+			}
+
+			SDL_RenderPresent(ren); //Actual point in the program where the screen is updated
+
+			if(!p && !fail) s1.update(dir); //update the snake and food location if game isn't paused and isn't in a fail state
+
+			int frame = SDL_GetTicks() - start;
+
+			if(frame < 1000/fps) //if the amount of time taken to render is less than one frame, delay until one frame's worth of time has passed
+				SDL_Delay((1000/fps - frame) + del);
 		}
-		SDL_SetRenderDrawColor(ren,0x98,0x80,0x4F,255); //Render Background
-		SDL_RenderClear(ren);
-
-		SDL_SetRenderDrawColor(ren,0x1D,0xC9,0x5C,255); //Set color to a light green
-
-		s1.render(ren); //Render snake, snake head and food
-
-		SDL_SetRenderDrawColor(ren,0,180,150,255);
-		SDL_RenderFillRect(ren,&bar);
-
-		char sc[15];
-
-		sprintf(sc,"Score  %d", s1.score());
-
-		s1.render(ren,font,&scol,surf,text,&score,&p,sc); // Render Score Text
-
-		char sp[20];
-
-		sprintf(sp,"Speed  %d",((del-200)/2)*(-1));
-
-		s1.render(ren,font,&scol,surf,text,&speed,&p,sp);
-
-		if(p) //if paused dim the screen
-		{
-			SDL_SetRenderDrawBlendMode(ren,SDL_BLENDMODE_BLEND);
-			SDL_SetRenderDrawColor(ren,0,0,0,100);
-			SDL_RenderFillRect(ren,&r);
-
-			s1.render(ren,pfont,&pcol,surf,text,&pause,&p,pz);
-		}
-
-		if(fail)
-		{
-			//Render Snake in red
-			s1.renderf(ren);
-
-			//Render a red overlay on the screen
-			SDL_SetRenderDrawBlendMode(ren,SDL_BLENDMODE_BLEND); 
-			SDL_SetRenderDrawColor(ren,120,0,0,100);
-			SDL_RenderFillRect(ren,&r);
-		}
-
-		SDL_RenderPresent(ren); //Actual point in the program where the screen is updated
-
-		if(fail)
-		{
-			//Wait for 1 second(1000ms) and exit the game
-			SDL_Delay(1000);
-			break;
-		}
-
-		if(!p && !fail) s1.update(dir); //update the snake and food location if game isn't paused and isn't in a fail state
-
-		int frame = SDL_GetTicks() - start;
-
-		if(frame < 1000/fps) //if the amount of time taken to render is less than one frame, delay until one frame's worth of time has passed
-			SDL_Delay((1000/fps - frame) + del);
+		if(cont) continue;
+		if(!run) break;
 	}
-
 	SDL_DestroyTexture(text);
 	SDL_FreeSurface(surf);
 	SDL_DestroyRenderer(ren);
 	SDL_DestroyWindow(win);
-
-	cout << "Score: " << s1.score() << "\n";
 
 	return 0;
 }
